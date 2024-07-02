909dd45b8685abf25bd22170f196a34cba9a2150
victoradepoju <victoradepoju30@gmail.com>
2024-07-02 19:53:49 +0100

    setup dependencies and docker container

---
+++ src/main/java/com/victor/task_management_with_security/TaskManagementWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.victor.task_management_with_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementWithSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementWithSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/task_management_with_security/TaskManagementWithSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.task_management_with_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementWithSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
